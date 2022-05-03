163a2ca4b4aa666f7476e30efdff9a8adb0d47e4
Marcos Hirazawa <hirazawa.marcos@gmail.com>
2022-05-03 16:43:02 -0300

    Deploy Vercel

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,5 @@ export default {
   url: 'https://strapi-postgres-mmh.herokuapp.com',
   siteName: 'Marcos Hirazawa',
   defaultSlug: 'landing-page',
-  graphqlURL: 'http://127.0.0.1:1337/graphql',
+  graphqlURL: 'https://strapi-postgres-mmh.herokuapp.com/graphql',
 };
--- src/templates/PostTemplate/index.tsx
+++ src/templates/PostTemplate/index.tsx
@@ -2,6 +2,7 @@ import { Post } from '../../components/Post';
 import { PostMetadata } from '../../shared-types/postAttributes';
 import { BaseTemplate } from '../Base';
 import { Settings } from '../../shared-types/settings';
+import { Comments } from '../../components/Comments';
 
 export type PostTemplateProps = {
   settings: Settings;
@@ -12,6 +13,12 @@ export const PostTemplate = ({ settings, post }: PostTemplateProps) => {
   return (
     <BaseTemplate settings={settings} showSearch={false}>
       <Post {...post} />
+      <Comments
+        allowComments={post.attributes.allowComments}
+        id={post.id}
+        title={post.attributes.title}
+        slug={post.attributes.slug}
+      />
     </BaseTemplate>
   );
 };
